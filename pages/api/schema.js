e1ff4d80d5693130af0e1666975a1a2879dcc97f
Amol Khatri <amolkhatri@Amols-MacBook-Pro.local>
2023-01-18 17:28:36 +0530

    Added prisma interaction

--- pages/api/schema.js
+++ pages/api/schema.js
@@ -1,14 +1,23 @@
 import { createSchema } from 'graphql-yoga'
- 
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient() 
+
 export const schema = createSchema({
   typeDefs: /* GraphQL */ `
+    type ToDo {
+      name: String
+    }
     type Query {
       hello: String
+      todos: [ToDo]
     }
   `,
   resolvers: {
     Query: {
-      hello: () => 'world'
+      hello: () => 'world',
+      todos: async () => {
+        return prisma.toDos.findMany();
+      }
     }
   }
 })
